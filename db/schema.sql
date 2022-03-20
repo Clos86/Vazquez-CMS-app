211f48b8faa36077fb268211eb3db006f5abffae
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-20 16:03:59 -0400

    edit schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10) NOT NULL,
   department_id INTEGER
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -20,4 +21,5 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
   manager_id INTEGER REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
