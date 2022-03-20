99c2e26c40a6459e5fa4cbd85fd6b3ee7824c3d0
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-20 18:06:58 -0400

    still working on it

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10) NOT NULL,
-  department_id INTEGER
+  department_id INTEGER,
   CONSTRAINT fk_department 
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
@@ -21,7 +21,7 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER REFERENCES employee(id)
+  manager_id INTEGER REFERENCES employee(id),
   CONSTRAINT fk_role 
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
