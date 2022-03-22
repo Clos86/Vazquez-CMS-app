fe244ed26c9add24ebafbc7f56bb66c28c697396
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-03-21 20:00:36 -0400

    fixed .env

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,27 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(30) NULL
 );
 
 CREATE TABLE roles (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10) NOT NULL,
-  department_id INTEGER,
-  CONSTRAINT fk_department 
-  FOREIGN KEY (department_id) REFERENCES departments(id)
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(30),
+     salary DECIMAL,
+     department_id INTEGER,
+     CONSTRAINT fk_department 
+     FOREIGN KEY (department_id) 
+     REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
-  manager_id INTEGER REFERENCES employee(id),
-  CONSTRAINT fk_role 
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30),
+     last_name VARCHAR(30),
+     role_id INTEGER,
+     manager_id INTEGER REFERENCES employees(id),
+     CONSTRAINT fk_role 
+     FOREIGN KEY (role_id) 
+     REFERENCES roles(id)
 );
